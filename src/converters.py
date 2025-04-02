76a08ad533793f39b7f891f91cfdea3f2bdc7931
foakes <afoakes@live.com>
2025-04-02 15:29:56 -0400

    text node conversion

--- src/converters.py
+++ src/converters.py
@@ -2,6 +2,19 @@ from textnode import TextNode, TextType
 from extractors import extract_markdown_links, extract_markdown_images
 
 
+def text_to_textnodes(text):
+    delimiters_dict = {"**": TextType.BOLD, "_": TextType.ITALIC, "`": TextType.CODE}
+    if text == "":
+        return []
+    raw_textnode = [TextNode(text, TextType.TEXT)]
+    raw_textnode = split_nodes_image(raw_textnode)
+    raw_textnode = split_nodes_link(raw_textnode)
+    for delimiter in delimiters_dict:
+        raw_textnode = split_nodes_delimiter(raw_textnode, delimiter, delimiters_dict[delimiter])
+    return raw_textnode
+
+
+
 def split_nodes_image(old_nodes):
     new_nodes = []
     for old_node in old_nodes:
@@ -58,26 +71,12 @@ def split_nodes_link(old_nodes):
     return new_nodes
 
 
-
-
-
-            
-    # print(image_tuple_list)
-    # print(counter)
-    # print(new_nodes)
-
-
-
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
         else:
-            if delimiter not in old_node.text:
-                raise ValueError(f"Delimiter '{delimiter}' not found in node: {old_node.text}")
-            if old_node.text.count(delimiter) % 2 != 0:
-                raise ValueError(f"Delimiter {delimiter} not closed")
             split_nodes = []
             split_text = old_node.text.split(delimiter)
             if len(split_text) % 2 == 0:
@@ -90,54 +89,5 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 else:
                     split_nodes.append(TextNode(split_text[i], text_type))
             new_nodes.extend(split_nodes)
-            
-            # ***Unnecessary, the expected TextType is already defined in the input function
-
-            # if delimiter == "**":
-            #     new_nodes.append(TextNode(split_node[1], TextType.BOLD))
-            # elif delimiter == "_":
-            #     new_nodes.append(TextNode(split_node[1], TextType.ITALIC))
-            # elif delimiter == "`":
-            #     new_nodes.append(TextNode(split_node[1], TextType.CODE))
-            
-            
     return new_nodes
-    #print(new_nodes)
-
-'''
-Some tests for Images/Links for convenience
-
-linknode = TextNode(
-    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
-      TextType.TEXT,
-        )
-imagenode = TextNode(
-        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
-        TextType.TEXT,
-    )
-oneimagenode = TextNode(
-    "This is text with anxx ![image](https://i.imgur.com/dfkjsdd.png) of a bird",
-    TextType.TEXT,
-)
-
-print(split_nodes_image([imagenode]))
-# split_nodes_delimiter(node, "**", TextType.BOLD)
-
-# node1 = TextNode("This **is** a test", TextType.TEXT)
-# node2 = TextNode("This is **a** test", TextType.TEXT)
-# print(split_nodes_delimiter([node1, node2], "**", TextType.BOLD))
-
-'''
-
-# def split_nodes_delimiter(old_nodes, delimiter, text_type):
-#     converted = []
-#     if old_nodes and isinstance(old_nodes, list):
-#         for node in range(0, len(old_nodes)):
-#             current_node = old_nodes[node].text.split(delimiter, maxsplit=1)
-#             new_node = TextNode(current_node, text_type)
-#             converted.append(split_nodes_delimiter(old_nodes[node+1:], delimiter, text_type))
-#     return converted
-        
-            
-
-
+    
